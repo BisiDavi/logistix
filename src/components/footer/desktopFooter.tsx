9157d10a130f026e8859fefd288077fb5607150f
BisiDavi <oludavidconnect@gmail.com>
2021-06-07 19:51:45 +0100

    footer fix

---
+++ src/components/footer/desktopFooter.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { Logo } from '@components/icons';
+import { displayLinks } from '@utils/.';
+import { menuLinks, policies, socialIcons } from './footerContent';
+import styles from '@/styles/footer.module.css';
+
+const DesktopFooter = () => {
+    return (
+        <>
+            <Row>
+                <Col xs={6}>
+                    <Logo />
+                </Col>
+            </Row>
+            <Row className='justify-content-between'>
+                <Col xs={5}>{displayLinks(menuLinks)}</Col>
+                <Col xs={4} className='px-0'>
+                    <div className='policies'>{displayLinks(policies)}</div>
+                </Col>
+                <Col className='d-flex flex-column align-items-center' xs={12}>
+                    <div className={styles.socialIcons}>
+                        {displayLinks(socialIcons)}
+                    </div>
+                    <div className='copyright'>
+                        <p>&copy; 2021 Logistix.</p>
+                    </div>
+                </Col>
+            </Row>
+        </>
+    );
+};
+
+export default DesktopFooter;
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,33 +1,11 @@
 import React, { FC } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { Logo } from '@components/icons';
-import { displayLinks } from '@utils/.';
-import { menuLinks, policies, socialIcons } from './footerContent';
-import styles from '../../styles/footer.module.css';
+import { Container } from 'react-bootstrap';
 import { BottomMenu } from '@components/menu';
+import styles from '../../styles/footer.module.css';
 
 const Footer: FC = () => {
     return (
         <Container as='footer' className={styles.footer} fluid>
-            {/*<Row>
-                <Col xs={6}>
-                    <Logo />
-                </Col>
-            </Row>
-            <Row className='justify-content-between'>
-                <Col xs={5}>{displayLinks(menuLinks)}</Col>
-                <Col xs={4} className='px-0'>
-                    <div className='policies'>{displayLinks(policies)}</div>
-                </Col>
-                <Col className='d-flex flex-column align-items-center' xs={12}>
-                    <div className={styles.socialIcons}>
-                        {displayLinks(socialIcons)}
-                    </div>
-                    <div className='copyright'>
-                        <p>&copy; 2021 Logistix.</p>
-                    </div>
-                </Col>
-            </Row>*/}
             <BottomMenu />
         </Container>
     );
