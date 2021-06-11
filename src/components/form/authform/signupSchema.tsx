5511b93033ab14cfe7be3dc98bdbe170d1f07920
BisiDavi <oludavidconnect@gmail.com>
2021-06-11 15:02:09 +0100

    main

---
+++ src/components/form/authform/signupSchema.tsx
@@ -0,0 +1,25 @@
+import * as Yup from 'yup';
+
+export const SignupSchema = Yup.object().shape({
+    fullName: Yup.string().required('your full name'),
+    email: Yup.string().email().required('your email'),
+    phoneNumber: Yup.string()
+        .required('Phone number is required')
+        .matches(
+            /^([0]{1}|\+?[234]{3})([7-9]{1})([0|1]{1})([\d]{1})([\d]{7})$/g,
+            'Invalid phone number',
+        ),
+    sex: Yup.string().required('your sex'),
+    location: Yup.string().required('select your delivery method'),
+    password: Yup.string()
+        .required('Your password')
+        .min(6, 'Password is too short - should be minimum of 6 letters'),
+    confirmPassword: Yup.string(),
+});
+
+export const LoginSchema = Yup.object().shape({
+    email: Yup.string().email().required('your email'),
+    password: Yup.string()
+        .required('Your password')
+        .min(6, 'Password is too short - should be minimum of 6 letters'),
+});
