b80416818439762047c8912a039a7d798125aaa1
BisiDavi <oludavidconnect@gmail.com>
2021-06-27 03:05:24 +0100

    main

---
+++ src/pages/api/order.js
@@ -0,0 +1,26 @@
+import nc from 'next-connect';
+import { all } from '@middlewares/.';
+
+const handler = nc();
+
+handler.use(all);
+
+handler.post(async (req, res) => {
+    const {
+        deliveryAddress,
+        parcel,
+        pickupAddress,
+        deliveryMethod,
+        state,
+    } = req.body;
+
+    const order = await saveOrder(req.db, {
+        deliveryAddress,
+        parcel,
+        pickupAddress,
+        deliveryMethod,
+        state,
+    });
+});
+
+export default handler;
