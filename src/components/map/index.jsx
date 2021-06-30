bfda80f7b510872d3b0f0491a8ac74b734db942b
BisiDavi <oludavidconnect@gmail.com>
2021-06-30 17:38:28 +0100

    main

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,15 +1,9 @@
-import React, { useState, useCallback } from 'react';
-import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
-import PuffLoader from 'react-spinners/PuffLoader';
+import React, { useState } from 'react';
+import { GoogleMap, Marker } from '@react-google-maps/api';
 import styles from '../../styles/map.module.css';
 
 function Map({ address, userLocation }) {
     const [map, setMap] = useState(null);
-
-    const { isLoaded, loadError } = useJsApiLoader({
-        id: 'google-map-script',
-        googleMapsApiKey: process.env.NEXT_PUBLIC_APIKEY,
-    });
 
     const containerStyle = {
         width: '100%',
@@ -27,20 +21,11 @@ function Map({ address, userLocation }) {
 
     console.log('google map', map);
 
-    function renderMap() {
-        const onLoad = useCallback(function callback(map) {
-            const bounds = new window.google.maps.LatLngBounds();
-            map.fitBounds(bounds);
-            setMap(map);
-            address(map);
-            return console.log('map: ', map);
-        }, []);
-
-        const onUnmount = useCallback(function callback(map) {
-            setMap(null);
-        }, []);
-
-        return (
+    return (
+        <LoadScript
+            libraries={['places']}
+            googleMapsApiKey={process.env.NEXT_PUBLIC_APIKEY}
+        >
             <GoogleMap
                 id='google-map'
                 mapContainerStyle={containerStyle}
@@ -52,16 +37,8 @@ function Map({ address, userLocation }) {
             >
                 <Marker draggable={true} onLoad={onLoad} position={position} />
             </GoogleMap>
-        );
-    }
-
-    if (loadError) {
-        return (
-            <div>Map cannot be loaded right now, please check your network</div>
-        );
-    }
-
-    return isLoaded ? renderMap() : <PuffLoader color={'#c85a5f14;'} />;
+        </LoadScript>
+    );
 }
 
 export default React.memo(Map);
